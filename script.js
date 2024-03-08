1bf621e0ee2b5496fb278c639a8749f395f0d33c
zaraamerr <zaraa.amerr@gmail.com>
2024-03-07 20:52:29 -0500

    add row function implemented

--- script.js
+++ script.js
@@ -5,7 +5,20 @@ let colorSelected;
 
 // Add a row
 function addR() {
-    alert("Clicked Add Row"); // Replace this line with your code.
+    let grid = document.getElementById('grid'); // Get the grid table reference
+    let row = grid.insertRow(0); // Insert a new row into the beginning of the grid
+    numRows++; // Increment numRows
+
+    if (numCols === 0) { //if there are no columns
+        let cell = row.insertCell(0); //add one cell at the beginning of the grid
+        numCols++; // Increment the global variable numCols
+        return; // Exit
+    }
+
+    // however, if there are existing columns, add cells accordingly
+    for (let i = 0; i < numCols; i++) {
+        let cell = row.insertCell(i);
+    }
 }
 
 // Add a column
