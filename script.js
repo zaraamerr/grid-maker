0085734b19e0f68b94605c5259934875cb479f6d
fatimasif <fatimasif2004@gmail.com>
2024-03-13 17:49:21 -0400

    added the remove column function and fixed remove row

--- script.js
+++ script.js
@@ -42,17 +42,45 @@ function addC() {
 // Remove a row
 function removeR() {
     let grid = document.getElementById('grid'); // Get the grid table reference
-   if(numRows > 0) {
-       grid.deleteRow(0); // Delete the first row
-       numRows--; // Decrement numRows
-   } else {
-         return;
-   }
+    if (numRows > 0) {
+        grid.deleteRow(0); // Delete the first row
+        numRows--; // Decrement numRows
+    } else {    
+        return;
+    }
+
+    // adjust the number of columns
+    if (numRows === 0) {
+        numCols = 0;
+    } else {
+        numCols = grid.rows[0].cells.length;
+    }
 }
 
 // Remove a column
 function removeC() {
-    alert("Clicked Remove Col"); // Replace this line with your code.
+    let grid = document.getElementById('grid'); // Get the grid table reference
+    // if there are no columns return
+    if (numCols === 0) {
+        return;
+    }
+    numCols--; // Decrement numCols
+    // Loop through all existing rows in the grid
+    for (let i = 0; i < grid.rows.length; i++) {
+        let row = grid.rows[i];
+        // delete the last cell in each row if it exists
+        if (row.cells.length > 0) {
+            row.deleteCell(row.cells.length - 1);
+        }
+    }
+    // adjust the number of rows
+    if (numCols === 0) {
+        numRows = 0;
+    } else {
+        numRows = grid.rows[0].cells.length;
+    }
+    // adjust the number of rows
+    // numRows = grid.rows.length;
 }
 
 // Set global variable for selected color
